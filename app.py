6737059f693da5cbce8054e1e43d4c3fe2a4b043
Suraj Kattige <suraj.kattige056@gmail.com>
2018-03-14 21:13:08 -0700

    Making changes to other files

--- app.py
+++ app.py
@@ -17,10 +17,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity) #This creates a new endpoint called /auth
 
 api.add_resource(Item, '/item/<string:name>')
@@ -30,6 +26,4 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == "__main__":
-    from db import db
-    db.init_app(app)
     app.run(port=5000)
